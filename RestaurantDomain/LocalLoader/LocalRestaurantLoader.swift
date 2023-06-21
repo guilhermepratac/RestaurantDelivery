3930e68280af124bf5b65b8c2458104f8f005127
Guilherme Prata Costa <guilherme.costa@invillia.com>
2023-06-21 18:58:18 -0300

    implementando camada LocalRestaurantLoader

--- RestaurantDomain/LocalLoader/LocalRestaurantLoader.swift
+++ RestaurantDomain/LocalLoader/LocalRestaurantLoader.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+/*
+ ### Salvar lista de restaurantes
+ 
+ #### Dados (Entrada):
+ - Listagem de restaurantes;
+ 
+ #### Curso primário (caminho feliz):
+ 1. Execute o comando "Salvar listagem de restaurantes" com os dados acima.
+ 2. O sistema deleta o cache antigo.
+ 3. O sistema codifica a lista de restaurantes.
+ 4. O sistema marca a hora do novo cache.
+ 5. O sistema salva o cache com novos dados.
+ 6. O sistema envia uma mensagem de sucesso.
+ 
+ #### Caso de erro (caminho triste):
+ 1. O sistema envia uma mensagem de erro.
+ 
+ #### Caso de erro ao salvar (caminho triste):
+ 1. O sistema envia uma mensagem de erro.
+ 
+ */
+
+protocol CacheClient {
+    func save(_ items: [RestaurantItem], timestamp: Date, completion: @escaping (Error?) -> Void)
+    func delete(completion: @escaping (Error?) -> Void)
+}
+
+final class LocalRestaurantLoader {
+    
+    let cache: CacheClient
+    let currentDate: () -> Date
+    
+    init(cache: CacheClient, currentDate: @escaping () -> Date) {
+        self.cache = cache
+        self.currentDate = currentDate
+    }
+    
+    func save(_ items: [RestaurantItem], completion: @escaping (Error?) -> Void) {
+        cache.delete { [unowned self] error in
+            if error == nil {
+                self.cache.save(items, timestamp: self.currentDate(), completion: completion)
+            } else {
+                completion(error)
+            }
+        }
+    }
+}
